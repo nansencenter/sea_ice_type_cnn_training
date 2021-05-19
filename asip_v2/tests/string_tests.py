67a3435e730e42c26627abff995d6065e5523c0b
azamifarda <arash.azamifard@nersc.no>
2021-05-19 14:27:44 +0200

    14 moving tests

--- asip_v2/tests/string_tests.py
+++ asip_v2/tests/string_tests.py
@@ -15,6 +15,3 @@ class TestStringMethods(unittest.TestCase):
         # check that s.split fails when the separator is not a string
         with self.assertRaises(TypeError):
             s.split(2)
-
-if __name__ == '__main__':
-    unittest.main()
