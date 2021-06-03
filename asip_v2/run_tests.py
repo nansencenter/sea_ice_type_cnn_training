b1364be823b4e2cd25aadaf6ad871c7a03656c95
azamifarda <arash.azamifard@nersc.no>
2021-06-03 15:41:19 +0200

    #14 new tests are addressed in the run_tests.py

--- asip_v2/run_tests.py
+++ asip_v2/run_tests.py
@@ -1,14 +1,15 @@
 import unittest
 
-from tests.test_utility import UtilityFunctionsTestCases, ConfigureTestCases
-from tests.test_build_dataset import BuildDatasetTestCases
-from tests.test_train_model import FileBasedConfigureTestCases
 from tests.test_apply_model import MemoryBasedConfigureTestCases
-
-#from tests.test_data_generator import DataGeneratorTestCases
-#from tests.test_archive import ArchiveTestCases
-
-
+from tests.test_archive import (Amsr2BatchesTestCases, ArchiveTestCases,
+                                BatchesTestCases, OutputBatchesTestCases,
+                                SarBatchesTestCases)
+from tests.test_build_dataset import BuildDatasetTestCases
+from tests.test_data_generator import (DataGeneratorFrom_npz_FileTestCases,
+                                       DataGeneratorFromMemoryTestCases,
+                                       DataGeneratorTestCases)
+from tests.test_train_model import FileBasedConfigureTestCases
+from tests.test_utility import ConfigureTestCases, UtilityFunctionsTestCases
 
 if __name__ == '__main__':
-    unittest.main(failfast=True)
+    unittest.main(failfast=True, buffer=True)
