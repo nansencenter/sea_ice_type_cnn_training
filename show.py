0591e251e6006faa7fb28f113e3f93f1c6e7baf5
azamifarda <arash.azamifard@nersc.no>
2021-06-04 10:40:38 +0200

    minor in show.py

--- show.py
+++ show.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
 import numpy as np
-reconstructed_arr = np.load("/abs/pat/to/npz/file")
+reconstructed_arr = np.load("/abs/pat/to/reconstructed/npz/file")
 plt.imshow(reconstructed_arr['arr_0'])
 plt.show()
