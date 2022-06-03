2445a72ec3b38998d7f5d18abecff3648652e65f
Anton Korosov <korosov@gmail.com>
2022-06-03 10:14:23 +0200

    clean up run_tests.py

--- asip_v2/run_tests.py
+++ asip_v2/run_tests.py
@@ -1,15 +1,6 @@
 import unittest
 
-from tests.test_apply_model import MemoryBasedConfigureTestCases
-from tests.test_archive import (Amsr2BatchesTestCases, ArchiveTestCases,
-                                BatchesTestCases, OutputBatchesTestCases,
-                                SarBatchesTestCases)
-from tests.test_build_dataset import BuildDatasetTestCases
-from tests.test_data_generator import (DataGeneratorFrom_npz_FileTestCases,
-                                       DataGeneratorFromMemoryTestCases,
-                                       DataGeneratorTestCases)
-from tests.test_train_model import FileBasedConfigureTestCases
-from tests.test_utility import ConfigureTestCases, UtilityFunctionsTestCases
+from tests.test_archive import BatchesTestCases
 
 if __name__ == '__main__':
     unittest.main(failfast=True, buffer=True)
