58f6f510f0f4b04d60c206e54bb92eabdc9ba981
Clement Fougerouse <clefou@maud.ad.nersc.no>
2022-08-19 16:40:03 +0200

    CF add comparaison netwok sum

--- asip_v2/data_generator.py
+++ asip_v2/data_generator.py
@@ -254,7 +254,160 @@ class HugoBinaryGenerator(HugoDataGenerator):
                 return [0,0,0,0]
         return r[:4]
     
+class DataGenerator_sod_f_all(DataGenerator):
+    def __init__(self, list_IDs, **kwargs):
+        super().__init__(list_IDs, **kwargs)
+        self.dims_input = (50,50,4)
+        self.nb_class = 32
+        self.dims_output = np.array([0]*self.nb_class).shape
+        
+    def ice_type(self, combi):
+        """
+        Transform the combiantion in a index for the one_hot_encodinf function       
+        
+        Parameters
+        ----------
+        combi : string
+            the combinaison of the sod and form 
+        Returns
+        -------
+        index_ : integer
+            index of the list where the value will be.
+        """
+        index_= -9
+        if combi =='0_0':
+            index_ = 0
+        if combi in ('82_2', '82_3', '82_4', '82_5', '83_3', '83_4'):
+            index_ = 1
+        if combi =='83_5':
+            index_ = 2
+        if combi in ('83_6', '87_3', '87_4', '87_5'):
+            index_ = 3
+        if combi =='87_6':
+            index_ = 4
+        if combi in ('91_2', '91_3', '91_4'):
+            index_ = 5
+        if combi =='91_5':
+            index_ = 6
+        if combi =='91_6':
+            index_ = 7
+        if combi =='91_7':
+            index_ = 8
+        if combi =='93_2':
+            index_ = 9
+        if combi in ('93_3', '93_4', '93_5'):
+            index_ = 10
+        if combi =='93_6':
+            index_ = 11
+        if combi =='93_7':
+            index_ = 12
+        if combi in ('95_3', '95_4'):
+            index_ = 13
+        if combi =='95_5':
+            index_ = 14
+        if combi in ('95_6', '95_7'):
+            index_ = 15
+        if combi in ('96_6', '97_7'):
+            index_ = 16
+        if index_ == -9:
+            index_ = 17
+        return index_   
+    
+    def one_hot_continous(self, vector_param):
+        """
+        Converts the output parameter vector ([ct,ca,sa,fa,...])
+        into a vector that contains the concentration percentages for the combinations.
 
+        Parameters
+        ----------
+        vector_param : list
+            all parameters in a vector.
+
+        Returns
+        -------
+        result : list
+            List of percentage concentrations for each work combination.
+        """
+
+        result = [0]*(self.nb_class)
+        vector_param = vector_param.squeeze()
+        print(vector_param)
+        if vector_param[0] < 10 : #open weter
+            combi = "0_0"
+            index_combi = self.ice_type(combi)
+            result[index_combi] = 1
+        for ice in range(3): # in a output there are 3 data for the 3 most present ice
+            if vector_param[1+ice*3] == (-9):
+                continue
+            if vector_param[2+ice*3] == (-9):
+                continue
+            if vector_param[3+ice*3] == (-9):
+                continue
+            combi = str(int(vector_param[2+ice*3])) + '_' + str(int(vector_param[3+ice*3]))
+            
+            index_combi = self.list_combi.index(combi)
+            result[index_combi] += vector_param[1+ice*3]/100
+        if max(result) == 0:
+            combi = str(int(vector_param[2])) + '_' + str(int(vector_param[3]))
+            index_combi = self.list_combi.index(combi)
+            result[index_combi] += round(vector_param[0]/100,1)
+            result[0] = 1-sum(result[1:])
+        else:
+            result[0] = 1-sum(result[1:])
+        result = self.convert(result)
+#         result = result[:self.nb_class]
+        print(result)
+        return np.round(result,1)
+
+    def __getitem__(self, index):
+        'Generate one batch of data'
+        # Generate indexes of the batch
+        indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
+
+        # Find list of IDs
+        self.list_IDs_temp = [self.list_IDs[k] for k in indexes]
+
+        # Generate data
+        self.data_generation()
+
+        return [self.X,self.z], self.y
+
+    def x_y_z_initialization(self):
+        # Initialization
+        self.X = np.empty((self.batch_size, *self.dims_input))
+        self.y = np.empty((self.batch_size, *self.dims_output))
+        self.z = np.empty((self.batch_size, *self.dims_amsr2))
+
+    def data_generation(self):
+        'Generates data containing batch_size samples' # X : (n_samples, *dim)
+
+        self.x_y_z_initialization()
+        
+        # Generate data
+        c = 0
+        for i, ID in enumerate(self.list_IDs_temp):
+            batch = {}
+            batch.update(np.load(ID))
+            vector_param = batch.get(self.output_var_name)            
+            output = self.one_hot_continous(vector_param)   
+
+            if sum(output) != 1 :
+                self.y = np.delete(self.y,(i-c), axis = 0)
+                self.X = np.delete(self.X,(i-c), axis = 0)
+                self.z = np.delete(self.z,(i-c), axis = 0)
+                c=c+1
+            else:
+                self.y[(i-c),:] = output
+                for j, sar_name in enumerate(self.input_var_names):
+                    sar = batch.get(sar_name)
+                    for k in range (2):
+                        self.X[(i-c),:,:,(j*2+k)] = sar[:,:,k]
+
+                for j, amsr2_name in enumerate(self.amsr2_var_names):
+                    with open(f'{self.idir_json}/stats_amsr2_test.json') as fichier_json:
+                        stats = json.load(fichier_json).get(amsr2_name)
+                    self.z[(i-c),:,:,j] = (batch.get(amsr2_name)-stats[0])/stats[1]
+                    
 class DataGenerator_sod_f(DataGenerator):
     def __init__(self, list_IDs, **kwargs):
         super().__init__(list_IDs, **kwargs)
